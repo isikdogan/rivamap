121067690ca469fe41f7c1afc1ef06887d1a024b
jay <jayaram.hariharan@utexas.edu>
2020-04-04 18:07:38 -0500

    add tests and travis

---
+++ tests/test_delineate.py
@@ -0,0 +1,35 @@
+import pytest
+
+import numpy as np
+from rivamap import delineate
+
+
+
+
+def test_extractCenterlines():
+    '''
+    Unit test for extractCenterlines function
+    '''
+    psi = np.zeros((5,5))
+    orient = np.zeros((5,5))
+    # put some values in dummy psi
+    psi[:,2] = 1
+    # expect max values in interior to be held
+    nms = delineate.extractCenterlines(orient,psi)
+    # make assertion
+    assert np.all(nms[1:4,2]) == np.all(psi[1:4,2])
+
+
+
+def test_thresholdCenterlines():
+    '''
+    Unit test for thresholdCenterlines function
+    '''
+    # setup dummy case
+    nms = np.zeros((5,5))
+    nms[1:4,1] = 0.5
+    nms[1:4,3] = 0.5
+    nms[1:4,2] = 1
+    c = delineate.thresholdCenterlines(nms,bimodal=False)
+    # make assertion
+    assert np.all(c[2,0:2]) == np.all([False,True])
